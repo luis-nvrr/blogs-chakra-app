234b8b40b2f49bcaaa13435578510c96175b7bcd
luis navarro <luisfnvrr@gmail.com>
2021-08-15 00:26:48 -0300

    add deploy config

---
+++ README.md
@@ -0,0 +1,16 @@
+# Blogs App
+
+## Heroku App
+https://blogsapp-luisnvrr.herokuapp.com/
+
+
+``user: admin``
+
+``password:admin``
+
+## Stack
+The Rect frontend was developed using ViteJs & ChakraUI.
+The backend uses ExpressJS.
+
+## About
+The project idea is based on a series of exercises from the FullStackOpen2021 challenge.
--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,14 @@ export default defineConfig({
   plugins: [reactRefresh()],
   server: {
     host: '0.0.0.0',
-    port: 3000
+    port: 3000,
+    proxy: {
+      '^/api/.*': {
+        target: 'http://localhost:3003',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   },
   resolve: {
     alias: {
